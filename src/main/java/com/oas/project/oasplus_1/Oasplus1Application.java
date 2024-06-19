63803bd2e46d57774dabc776dc19576ccd164adf
Avadhut Patil <avadhutcpatil@gmail.com>
2024-06-19 15:40:46 +0530

    First Commit

---
+++ src/main/java/com/oas/project/oasplus_1/Oasplus1Application.java
@@ -0,0 +1,13 @@
+package com.oas.project.oasplus_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oasplus1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oasplus1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oas/project/oasplus_1/Oasplus1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oas.project.oasplus_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oasplus1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
